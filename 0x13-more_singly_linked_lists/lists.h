21d21f044b40611b8fd75c4ca278226f75aede36
john feyisayo <johnfeyisayo@gmail.com>
2022-04-25 23:08:40 +0100

    My commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,20 +1,23 @@
 #ifndef _LISTS_
 #define _LISTS_
+
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for ALX low level programming project
+ * for ALX low level programming  project
  */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
+
 /**
  * struct listp_s - singly linked list
  * @p: pointers of nodes
@@ -24,9 +27,10 @@ struct listint_s *next;
  */
 typedef struct listp_s
 {
-void *p;
-struct listp_s *next;
+	void *p;
+	struct listp_s *next;
 } listp_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -42,4 +46,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned inead);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
 #endif
